deea106187be67631accf2e9dbca013c1c09ecd8
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-04-03 19:39:07 +0700

    docker image

--- docker-image.sh
+++ docker-image.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Menampilkan daftar semua Docker image yang ada di local
 docker image ls
+
+# Mengunduh (pull) image Redis versi terbaru (tag: latest) dari Docker Hub
+docker image pull redis:latest
+
+# Menghapus image Redis dengan tag latest dari local
+docker image rm redis:latest
