da0547a2ce78fb4d33ceb93455d6cb3c0a03b053
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-04-04 16:02:13 +0700

    container env

---
+++ docker-container-env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Mengambil (download) image MongoDB versi terbaru dari Docker Hub
+docker image pull mongo:latest
+
+# Membuat container MongoDB dengan nama "contohmongo"
+$ docker container create --name contohmongo --publish 27017:27017 --env MONGO_INITDB_ROOT_USERNAME=elham --env MONGO_INITDB_ROOT_PASSWORD=elham mongo:latest
+
+docker container create \
+  --name contohmongo \
+
+  # Mapping port:
+  # port 27017 di host → port 27017 di container (default MongoDB)
+  --publish 27017:27017 \
+
+  # Set username untuk user root MongoDB
+  --env MONGO_INITDB_ROOT_USERNAME=elham \
+
+  # Set password untuk user root MongoDB
+  --env MONGO_INITDB_ROOT_PASSWORD=elham \
+
+  # Image yang digunakan
+  mongo:latest
--- docker-container-port.sh
+++ docker-container-port.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Mengambil (download) image NGINX versi terbaru dari Docker Hub
 docker image pull nginx:latest
 
+# Membuat container baru dengan nama "contohnginx"
+# --publish 8080:80 artinya:
+#   port 8080 di host (komputer kita)
+#   diarahkan ke port 80 di dalam container (NGINX default)
 docker container create --name contohnginx --publish 8080:80 nginx:latest
