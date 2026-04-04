f25dc2a7ee3f7b089f5e0602d717470a2231ce02
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-04-04 08:32:19 +0700

    docker container

---
+++ docker-container.sh
@@ -0,0 +1,5 @@
+# Menampilkan semua container (baik yang sedang berjalan maupun yang sudah berhenti)
+docker container ls -a
+
+# Menampilkan hanya container yang sedang berjalan (running saja)
+docker container ls
