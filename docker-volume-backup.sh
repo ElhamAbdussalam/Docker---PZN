2ca8c1a9c39280e6945322a104bbb646af675fc9
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-04-06 14:23:42 +0700

    backup volume

--- docker-volume-backup.sh
+++ docker-volume-backup.sh
@@ -1,15 +1,39 @@
+# Menghentikan container MongoDB (mongovolume)
+# Tujuannya agar data konsisten saat dibackup (tidak sedang ditulis)
 docker container stop mongovolume
 
-MSYS_NO_PATHCONV=1 docker container create --name nginxbackup --mount "type=bind,source=/c/Users/muham/Documents/Web/Docker - PZN/backup,destination=/backup" --mount "type=volume,source=mongodata,destination=/data" nginx:latest
+# Membuat container sementara "nginxbackup"
+# MSYS_NO_PATHCONV=1 -> mencegah Git Bash mengubah path
+MSYS_NO_PATHCONV=1 docker container create --name nginxbackup \
 
+  # Bind mount ke folder lokal untuk menyimpan file backup
+  --mount "type=bind,source=/c/Users/muham/Documents/Web/Docker - PZN/backup,destination=/backup" \
+
+  # Mount volume MongoDB agar bisa diakses dari container ini
+  --mount "type=volume,source=mongodata,destination=/data" \
+
+  # Menggunakan image nginx (sebagai container helper)
+  nginx:latest
+
+# Menjalankan container backup
 docker container start nginxbackup
 
+# Masuk ke dalam container
+# Gunakan /bin/sh jika /bin/bash tidak tersedia
 MSYS_NO_PATHCONV=1 docker container exec -it nginxbackup /bin/bash
 
+# Membuat file backup dalam bentuk tar.gz
+# /data   -> sumber data (volume MongoDB)
+# /backup -> tujuan (folder lokal)
 tar cvf /backup/backup.tar.gz /data
 
+# Keluar dari container (ketik: exit), lalu lanjut:
+
+# Menghentikan container backup
 docker container stop nginxbackup
 
+# Menghapus container backup (karena hanya sementara)
 docker container rm nginxbackup
 
+# Menjalankan kembali MongoDB
 docker container start mongovolume
