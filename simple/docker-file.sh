151d14aee581bc071d03e43903496e22f168caca
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-04-07 08:20:11 +0700

    CMD

--- simple/docker-file.sh
+++ simple/docker-file.sh
@@ -1,3 +1,11 @@
 docker build -t elham/simple simple
 
 docker build -t elham/run run
+
+docker build -t elham/command command
+
+docker container create --name command elham/command
+
+docker container start command
+
+docker container logs command
